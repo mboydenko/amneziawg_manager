901b1b3d769e62b951c9cdc666855cf7df012e00
Maksim Boydenko <boidenko.maxim@gmail.com>
2026-05-25 13:12:59 +0300

    fix keys generations

--- amneziawg_manager/lib/amnezia_wg_manager.py
+++ amneziawg_manager/lib/amnezia_wg_manager.py
@@ -226,7 +226,7 @@ class AmneziaWgManager:
         Return tuple: public, privet, preshared 
         """
         tmp_dir = f"~/{datetime.now().strftime('%Y%m%d%H%M%S')}_tmp"
-        self.cmd_executor.exec_cmd(f'mkdir ${tmp_dir}')
+        self.cmd_executor.exec_cmd(f'mkdir {tmp_dir}')
         try:
             self.cmd_executor.exec_cmd(f'wg genkey > {tmp_dir}/private.key')
             private_key = self.cmd_executor.exec_cmd(f'cat {tmp_dir}/private.key').stdout
